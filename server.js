aeed04d0a56008877b05070eed103b8a3b03516a
Reem <reembsrat@gmail.com>
2022-09-26 16:55:52 +0300

    my first commit

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const connectDB = require('./config/database')
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './config/.env'}) //allows us to use env file
 
 connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true })) //this line allows us to use the req body
 app.use(express.json())
 
 app.use('/', homeRoutes)
