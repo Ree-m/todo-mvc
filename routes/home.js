0b00373eda3ee27b4ca02275a0a39f3d5bc61252
Reem <reembsrat@gmail.com>
2022-09-27 23:49:02 +0300

    fixed server

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express') //get express? so we use this everywhere?
+const router = express.Router() //get router
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex) 
 
-module.exports = router
+module.exports = router  //export allow this file to be used everywhere.
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const app = express()
-const connectDB = require('./config/database')
+const app = express()  //app is now express
+const connectDB = require('./config/database') 
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
@@ -12,6 +12,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true })) //this line allows us to use the req body
 app.use(express.json())
+
+
 
 app.use('/', homeRoutes)
 app.use('/todos', todoRoutes)
